c8933edef257d4bff350f7ba6f7b83e71bdcb08d
Cédric Belin <cedric@belin.io>
2020-09-24 16:53:11 +0200

    Added the externs of the `Clipboard` class

---
+++ src/js/nw/Clipboard.hx
@@ -0,0 +1,57 @@
+package js.nw;
+
+/** An abstraction of the clipboard. **/
+@:native("nw.Clipboard")
+extern class Clipboard {
+
+	/** Returns the clipboard object. **/
+	static function get(): Clipboard;
+
+	/** Clears the clipboard. **/
+	function clear(): Void;
+
+	/** Gets the data from the clipboard. **/
+	@:overload(function(data: ClipboardData): String {})
+	@:overload(function(dataList: Array<ClipboardData>): String {})
+	function get(type: ClipboardDataType = Text, raw: Bool = false): String;
+
+	/** Returns the list of available types of data in clipboard. **/
+	function readAvailableTypes(): Array<ClipboardDataType>;
+
+	/** Writes the specified `data` to the clipboard. **/
+	@:overload(function(data: ClipboardData): Void {})
+	@:overload(function(dataList: Array<ClipboardData>): Void {})
+	function set(data: String, type: ClipboardDataType = Text, raw: Bool = false): Void;
+}
+
+/** Defines the options of clipboard data. **/
+typedef ClipboardData = {
+
+	/** The clipboard data. **/
+	var data: String;
+
+	/** Value indicating whether to require raw image data. **/
+	var ?raw: Bool;
+
+	/** The type of clipboard data.  **/
+	var ?type: ClipboardDataType;
+}
+
+/** Defines the type of clipboard data. **/
+enum abstract ClipboardDataType(String) from String to String {
+
+	/** The clipboard data is an HTML string. **/
+	var Html = "html";
+
+	/** The clipboard data is a JPEG image. **/
+	var Jpeg = "jpeg";
+
+	/** The clipboard data is a PNG image. **/
+	var Png = "png";
+
+	/** The clipboard data is Rich Text Format. **/
+	var Rtf = "rtf";
+
+	/** The clipboard data is plain text. **/
+	var Text = "text";
+}
