dcfaa98da0a4ac74c7d7e70c31546abf535e3317
Cédric Belin <cedric@belin.io>
2020-09-24 16:53:24 +0200

    Added the externs of the `MenuItem` class

---
+++ src/js/nw/MenuItem.hx
@@ -0,0 +1,99 @@
+package js.nw;
+
+/** Represents an item in a menu. **/
+@:native("nw.MenuItem")
+extern class MenuItem {
+
+	/** Value indicating whether the checkbox is checked. **/
+	var checked: Bool;
+
+	/** Value indicating whether this item is enabled. **/
+	var enabled: Bool;
+
+	/** The item icon. **/
+	var icon: String;
+
+	/** Value indicating whether the item icon is treated as a template. **/
+	var iconIsTemplate: Bool;
+
+	/** The key of the shortcut. **/
+	var key: String;
+
+	/** The item label. **/
+	var label: String;
+
+	/** The modifiers of the shortcut. **/
+	var modifiers: String;
+
+	/** The item submenu. **/
+	var submenu: Menu;
+
+	/** The item tooltip. **/
+	var tooltip: String;
+
+	/** The item type. **/
+	final type: MenuItemType;
+
+	/** Creates a new menu item. **/
+	function new(options: MenuItemOptions);
+
+	/** The callback invoked when the user activates this item. **/
+	dynamic function click(): Void;
+
+	/** Sets up an `handler` that will be invoked whenever the specified `event` is delivered to this object. **/
+	function on(event: MenuItemEvent, handler: () -> Void): Void;
+}
+
+/** Defines the events of a `MenuItem` instance. **/
+enum abstract MenuItemEvent(String) from String to String {
+
+	/** The `click` event. **/
+	var Click = "click";
+}
+
+/** Defines the options of a `MenuItem` instance. **/
+typedef MenuItemOptions = {
+
+	/** Value indicating whether the checkbox is checked. **/
+	var ?checked: Bool;
+
+	/** The callback to invoke when the user activates the item. **/
+	var ?click: () -> Void;
+
+	/** Value indicating whether the item is enabled. **/
+	var ?enabled: Bool;
+
+	/** The item icon. **/
+	var ?icon: String;
+
+	/** The key of the shortcut. **/
+	var ?key: String;
+
+	/** The item label. **/
+	var ?label: String;
+
+	/** The modifiers of the shortcut. **/
+	var ?modifiers: String;
+
+	/** The item submenu. **/
+	var ?submenu: Menu;
+
+	/** The item tooltip. **/
+	var ?tooltip: String;
+
+	/** The item type. **/
+	var ?type: MenuItemType;
+}
+
+/** Defines the type of a menu item. **/
+enum abstract MenuItemType(String) from String to String {
+
+	/** The menu item has a checkbox. **/
+	var Checkbox = "checkbox";
+
+	/** The menu item is standard. **/
+	var Normal = "normal";
+
+	/** The menu item is a separator. **/
+	var Separator = "separator";
+}
