105f60a7cf852712c7e54cc2a435f9c9bb3779cd
Cédric Belin <cedric@belin.io>
2020-09-26 19:05:13 +0200

    Merged the `Nw` and `NwFlavor` modules

--- src/js/Nw.hx
+++ src/js/Nw.hx
@@ -1,7 +1,32 @@
 package js;
 
+import js.Node.process;
+
 /** The top-level NW.js object. **/
 @:native("nw")
 extern class Nw {
 
+	/** The build flavor of NW.js. **/
+	static var flavor(get, never): NwFlavor;
+
+	/** The version number of NW.js. **/
+	static var version(get, never): String;
+
+	/** Gets the build flavor of NW.js. **/
+	static inline function get_flavor(): NwFlavor
+		return process.versions["nw-flavor"];
+
+	/** Gets the version number of NW.js. **/
+	static inline function get_version(): String
+		return process.versions["nw"];
+}
+
+/** Defines the NW.js build flavors. **/
+enum abstract NwFlavor(String) from String {
+
+	/** The NW.js binary is a normal build. **/
+	var Normal = "normal";
+
+	/** The NW.js binary is an SDK build. **/
+	var Sdk = "sdk";
 }
