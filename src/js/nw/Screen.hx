4685aee618d545970bf566a095ac1f7690bcdd08
Cédric Belin <cedric@belin.io>
2020-09-27 19:41:40 +0200

    Added the `js.nw.Screen` module

--- src/js/nw/Screen.hx
+++ src/js/nw/Screen.hx
@@ -1,35 +1,61 @@
 package js.nw;
 
+import js.node.events.EventEmitter;
 import haxe.Constraints.Function;
+import haxe.extern.EitherType;
 import js.node.events.EventEmitter.Event;
 
-/** TODO **/
-@:native("nw.Screen.DesktopCaptureMonitor")
-extern class DesktopCaptureMonitor {
-	// TODO
+/** Monitors the changes of screens and windows on the desktop. **/
+extern class DesktopCaptureMonitor extends EventEmitter<DesktopCaptureMonitor> {
 
-	/** Adds a `listener` that will be invoked whenever the specified `event` is delivered to this object. **/
-	// @:overload(function(event: DesktopCaptureMonitorEvent, handler: Null<IFrameElement> -> Void): Void {})
-	// function on(event: DesktopCaptureMonitorEvent, handler: ScreenDefinition -> Void): Void;
+	/** Value indicating whether the dektop capture monitor is started. **/
+	final started: Bool;
+
+	/** Registers and returns a valid stream identifier. **/
+	function registerStream(id: String): Void;
+
+	/** Starts monitoring the system and triggering the events. **/
+	function start(shouldIncludeScreens: Bool, shouldIncludeWindows: Bool): Void;
+
+	/** Stops monitoring the system. **/
+	function stop(): Void;
 }
 
+/** Defines the events of a `DesktopCaptureMonitor` instance. **/
 enum abstract DesktopCaptureMonitorEvent<T: Function>(Event<T>) to Event<T> {
 
+	/** The `added` event. **/
+	var Added: DesktopCaptureMonitorEvent<(String, String, Int, String, ?Bool) -> Void> = "added";
+
+	/** The `namechanged` event. **/
+	var NameChanged: DesktopCaptureMonitorEvent<(String, String) -> Void> = "namechanged";
+
+	/** The `orderchanged` event. **/
+	var OrderChanged: DesktopCaptureMonitorEvent<(String, Int, Int) -> Void> = "orderchanged";
+
+	/** The `removed` event. **/
+	var Removed: DesktopCaptureMonitorEvent<Int -> Void> = "removed";
+
+	/** The `thumbnailchanged` event. **/
+	var ThumbnailChanged: DesktopCaptureMonitorEvent<(String, String) -> Void> = "thumbnailchanged";
 }
 
-/** TODO **/
+/** Defines the desktop media sources used in `Screen.chooseDesktopMedia()` method. **/
 enum abstract DesktopMediaSource(String) {
 
-	/** TODO **/
+	/** The media source is a screen. **/
 	var Screen = "screen";
 
-	/** TODO **/
+	/** The media source is a window. **/
 	var Window = "window";
 }
 
 /** A collection of APIs to query the screens connected to the computer. **/
 @:native("nw.Screen")
 extern class Screen {
+
+	/** The object used to monitor the changes of screens and windows on the desktop. **/
+	final DesktopCaptureMonitor: DesktopCaptureMonitor;
 
 	/** The list of screens connected to the computer. **/
 	final screens: Array<ScreenDefinition>;
@@ -38,19 +64,22 @@ extern class Screen {
 	static function Init(): Void;
 
 	/** Adds a `listener` that will be invoked whenever the specified `event` is triggered. **/
-	static function addListener<T: Function>(event: Event<T>, listener: T): App;
+	static function addListener<T: Function>(event: Event<T>, listener: T): Screen;
+
+	/** Shows the desktop media picker UI with the specified set of `sources`. **/
+	static function chooseDesktopMedia(sources: Array<DesktopMediaSource>, callback: EitherType<Bool, String> -> Void): Void;
 
 	/** Removes the specified `listener` for the given `event`. **/
-	static function off<T: Function>(event: Event<T>, listener: T): App;
+	static function off<T: Function>(event: Event<T>, listener: T): Screen;
 
 	/** Adds a `listener` that will be invoked whenever the specified `event` is triggered. **/
-	static function on<T: Function>(event: Event<T>, listener: T): App;
+	static function on<T: Function>(event: Event<T>, listener: T): Screen;
 
 	/** Adds a one-time `listener` that will be removed and then invoked whenever the next time `event` is triggered. **/
-	static function once<T: Function>(event: Event<T>, listener: T): App;
+	static function once<T: Function>(event: Event<T>, listener: T): Screen;
 
 	/** Removes the specified `listener` for the given `event`. **/
-	static function removeListener<T: Function>(event: Event<T>, listener: T): App;
+	static function removeListener<T: Function>(event: Event<T>, listener: T): Screen;
 }
 
 /** Defines the structure of a screen. **/
