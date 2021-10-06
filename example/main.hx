5449cc4dae0b7c02c9993fdc29c562e434cae17b
Cédric Belin <cedric@belin.io>
2021-10-06 16:56:10 +0200

    Updated the example code

--- example/main.hx
+++ example/main.hx
@@ -16,128 +16,124 @@ import js.nw.Window;
 
 using StringTools;
 
-/** A sample class. **/
-class Example {
+/** Creates a native context menu. **/
+function createContextMenu() {
+	final contextMenu = new Menu();
+	contextMenu.append(new MenuItem({label: "Item A"}));
+	contextMenu.append(MenuItem.newSeparator());
+	contextMenu.append(new MenuItem({
+		type: Checkbox,
+		label: "Item B",
+		icon: "img/icon.png",
+		key: "s",
+		modifiers: "ctrl+alt",
+		click: () -> alert("You have activated 'Item B'")
+	}));
 
-	/** Creates a native context menu. **/
-	public function createContextMenu() {
-		final contextMenu = new Menu();
-		contextMenu.append(new MenuItem({label: "Item A"}));
-		contextMenu.append(MenuItem.newSeparator());
-		contextMenu.append(new MenuItem({
-			type: Checkbox,
-			label: "Item B",
-			icon: "img/icon.png",
-			key: "s",
-			modifiers: "ctrl+alt",
-			click: () -> alert("You have activated 'Item B'")
-		}));
+	document.body.addEventListener("contextmenu", event -> {
+		event.preventDefault();
+		contextMenu.popup(event.x, event.y);
+	});
+}
 
-		document.body.addEventListener("contextmenu", event -> {
-			event.preventDefault();
-			contextMenu.popup(event.x, event.y);
-		});
-	}
+/** Creates and sets the menubar. **/
+function createMenubar() {
+	final submenu = new Menu();
+	submenu.append(new MenuItem({label: "Item A"}));
+	submenu.append(new MenuItem({label: "Item B"}));
 
-	/** Creates and sets the menubar. **/
-	public function createMenubar() {
-		final submenu = new Menu();
-		submenu.append(new MenuItem({label: "Item A"}));
-		submenu.append(new MenuItem({label: "Item B"}));
+	final menubar = Menu.newMenubar();
+	if (process.platform == "darwin") menubar.createMacBuiltin("My App");
+	menubar.append(new MenuItem({
+		label: "First Menu",
+		submenu: submenu
+	}));
 
-		final menubar = Menu.newMenubar();
-		if (process.platform == "darwin") menubar.createMacBuiltin("My App");
-		menubar.append(new MenuItem({
-			label: "First Menu",
-			submenu: submenu
-		}));
+	Window.get().menu = menubar;
+}
 
-		Window.get().menu = menubar;
-	}
+/** Adds a system tray icon to the notification area. **/
+function createTray() {
+	final menu = new Menu();
+	menu.append(new MenuItem({type: Checkbox, label: "Box 1"}));
 
-	/** Adds a system tray icon to the notification area. **/
-	public function createTray() {
-		final menu = new Menu();
-		menu.append(new MenuItem({type: Checkbox, label: "Box 1"}));
+	var tray = new Tray({
+		title: "Tray",
+		icon: "img/icon.png",
+		menu: menu
+	});
 
-		var tray = new Tray({
-			title: "Tray",
-			icon: "img/icon.png",
-			menu: menu
-		});
+	tray.on(TrayEvent.Click, () -> {
+		tray.remove();
+		tray = null;
+	});
+}
 
-		tray.on(TrayEvent.Click, () -> {
-			tray.remove();
-			tray = null;
-		});
-	}
+/** Lists the version strings of NW.js. **/
+function getVersions() {
+	trace(Nw.flavor); // The NW.js flavor.
+	trace(Nw.version); // The NW.js version.
+	trace(process.versions["chromium"]); // The Chromium version.
+	trace(process.versions["node"]); // The Node.js version.
+}
 
-	/** Lists the version strings of NW.js. **/
-	public function getVersions() {
-		trace(Nw.flavor); // The NW.js flavor.
-		trace(Nw.version); // The NW.js version.
-		trace(process.versions["chromium"]); // The Chromium version.
-		trace(process.versions["node"]); // The Node.js version.
-	}
+/** Opens a new window. **/
+function openWindow() {
+	Window.open("https://nwjs.io", newWindow -> {
+		newWindow.on(Closed, () -> newWindow = null);
+		newWindow.on(Focus, () -> trace('New window is focused'));
+	});
+}
 
-	/** Opens a new window. **/
-	public function openWindow() {
-		Window.open("https://nwjs.io", newWindow -> {
-			newWindow.on(Closed, () -> newWindow = null);
-			newWindow.on(Focus, () -> trace('New window is focused'));
-		});
-	}
+/** Reads a file selected by the user with Node.js API. **/
+function readFileFromNode() {
+	final fileInput = cast(document.querySelector('input[type="file"]'), InputElement);
+	final filePath = fileInput.value;
+	Fs.readFile(filePath, "utf8", (error, text) -> {
+		if (error != null) console.error(error);
+		else trace(text);
+	});
+}
 
-	/** Reads a file selected by the user with Node.js API. **/
-	public function readFileFromNode() {
-		final fileInput = cast(document.querySelector('input[type="file"]'), InputElement);
-		final filePath = fileInput.value;
-		Fs.readFile(filePath, "utf8", (error, text) -> {
-			if (error != null) console.error(error);
-			else trace(text);
-		});
-	}
+/** Reads the native path of files selected by the user. **/
+function readFilePaths() {
+	final fileInput = cast(document.querySelector('input[type="file"]'), InputElement);
+	final fileList = fileInput.files;
+	for (index in 0...fileList.length) trace(untyped fileList[index].path);
+}
 
-	/** Reads the native path of files selected by the user. **/
-	public function readFilePaths() {
-		final fileInput = cast(document.querySelector('input[type="file"]'), InputElement);
-		final fileList = fileInput.files;
-		for (index in 0...fileList.length) trace(untyped fileList[index].path);
-	}
+/** Registers a global keyboard shortcut. **/
+function registerShortcut() {
+	final shortcut = new Shortcut({key : "Ctrl+Shift+A"});
+	shortcut.on(Active, () -> {
+		final self: Shortcut = nativeThis;
+		trace('Global desktop keyboard shortcut: ${self.key} active.');
+	});
 
-	/** Registers a global keyboard shortcut. **/
-	public function registerShortcut() {
-		final shortcut = new Shortcut({key : "Ctrl+Shift+A"});
-		shortcut.on(Active, () -> {
-			final self: Shortcut = nativeThis;
-			trace('Global desktop keyboard shortcut: ${self.key} active.');
-		});
+	App.registerGlobalHotKey(shortcut);
+}
 
-		App.registerGlobalHotKey(shortcut);
-	}
+/** Reads and writes data to the clipboard. **/
+function useClipboard() {
+	final clipboard = Clipboard.get();
+	clipboard.set("I love NW.js :)", Text);
+	trace(clipboard.get(Text));
 
-	/** Reads and writes data to the clipboard. **/
-	public function useClipboard() {
-		final clipboard = Clipboard.get();
-		clipboard.set("I love NW.js :)", Text);
-		trace(clipboard.get(Text));
+	final imagePath = Path.resolve("nw.png");
+	final imageData = Fs.readFileSync(imagePath).toString("base64");
+	final clipboardData: Array<ClipboardData> = [
+		{type: Html, data: '<img src="file://${imagePath.urlEncode()}"/>'},
+		{type: Png, data: imageData, raw: true}
+	];
 
-		final imagePath = Path.resolve("nw.png");
-		final imageData = Fs.readFileSync(imagePath).toString("base64");
-		final clipboardData: Array<ClipboardData> = [
-			{type: Html, data: '<img src="file://${imagePath.urlEncode()}"/>'},
-			{type: Png, data: imageData, raw: true}
-		];
+	clipboard.set(clipboardData);
+	trace(clipboard.get(Html));
+	clipboard.clear();
+}
 
-		clipboard.set(clipboardData);
-		trace(clipboard.get(Html));
-		clipboard.clear();
-	}
-
-	/** Uses the shell to perform desktop related jobs. **/
-	public function useShell() {
-		Shell.openExternal("https://nwjs.io"); // Open a URL with the default browser.
-		Shell.openItem("test.txt"); // Open a text file with the default text editor.
-		Shell.showItemInFolder("test.txt"); // Show a file in its parent folder with the file manager.
-	}
+/** Uses the shell to perform desktop related jobs. **/
+function useShell() {
+	Shell.openExternal("https://nwjs.io"); // Open a URL with the default browser.
+	Shell.openItem("test.txt"); // Open a text file with the default text editor.
+	Shell.showItemInFolder("test.txt"); // Show a file in its parent folder with the file manager.
 }
