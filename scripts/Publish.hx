eeb7842165e6f4a2effb944f82bf4bc45b3586a5
Cédric Belin <cedric@belin.io>
2023-02-12 01:29:35 +0100

    Fixed the "Publish" task [skip ci]

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -5,5 +5,5 @@ import nwjs.Version;
 function main() {
 	Tools.compress(["CHANGELOG.md", "LICENSE.md", "README.md", "haxelib.json", "src"], "var/haxelib.zip");
 	Sys.command("haxelib", ["submit", "var/haxelib.zip"]);
-	for (action in ["tag", "push origin"]) Sys.command("git", [action, 'v${Version.packageVersion}'])
+	for (action in [["tag"], ["push", "origin"]]) Sys.command("git", action.concat(['v${Version.packageVersion}']);
 }
