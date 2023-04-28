208b50995c252d065d4ee9425b12665e0fccf0e5
Cédric Belin <cedric@belin.io>
2023-04-29 01:02:08 +0200

    Avoid safe casts

--- example/ReadFileFromNode.hx
+++ example/ReadFileFromNode.hx
@@ -4,7 +4,7 @@ import js.node.Fs;
 
 /** Reads a file selected by the user with Node.js API. **/
 function main() {
-	final fileInput = cast(Browser.document.querySelector('input[type="file"]'), InputElement);
+	final fileInput = (cast Browser.document.querySelector('input[type="file"]'): InputElement);
 	final filePath = fileInput.value;
 	Fs.readFile(filePath, "utf8", (error, text) -> {
 		if (error != null) Browser.console.error(error);
