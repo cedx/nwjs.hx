637e19a0f188c79fb973857875956104782f5da0
Cédric Belin <cedric@belin.io>
2024-09-21 16:07:57 +0200

    Fix the "Publish" script [skip ci]

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -11,7 +11,6 @@ using haxe.zip.Tools;
 
 /** Publishes the package. **/
 function main() {
-	Sys.command("haxe run.hxml");
 	compress(["CHANGELOG.md", "LICENSE.md", "README.md", "haxelib.json", "run.n", "src"], "var/haxelib.zip");
 	Sys.command("haxelib submit var/haxelib.zip");
 	for (action in ["tag", "push origin"]) Sys.command('git $action v${Platform.packageVersion}');
